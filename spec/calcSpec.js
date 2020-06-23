04f646fcf2316efdd03dbe6d4d99774a430a0413
Vera Leitao <veraleitao.dev@gmail.com>
2020-06-23 20:30:00 +0000

    added 3rd spec to calcSpec.js and reviewed the addition function on calc.js

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,12 +1,15 @@
-  
 describe("Calculator", function() {
     describe("Addition function", function() {
         it("should return 42", function() {
             expect(addition(20, 22)).toBe(42);
         });
 
-        it("should not return 4 if the arguments given don't produce a sum of 4", function() {
+        it("should return 26", function() {
             expect(addition(7, 19)).toBe(26);
         });
+
+        it("should return an error if we don't supply two numbers", function() {
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!")
+        });
     });
-})
+});
